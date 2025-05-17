1faac8f3059287677bc462f1751892b94f18fc4e
Eric Xiao <ericxiao0000@gmail.com>
2025-05-16 23:33:26 -0700

    feat: catch typst syntax errors

--- src/SvgPreview.tsx
+++ src/SvgPreview.tsx
@@ -1,10 +1,27 @@
-import { Accessor, type Component, createResource } from "solid-js";
+import {
+  Accessor,
+  type Component,
+  createResource,
+  createSignal,
+} from "solid-js";
 
 import { $typst } from "@myriaddreamin/typst.ts";
 
+let oldSvg = "";
+
 const SvgPreview: Component<{ contentSignal: Accessor<string> }> = (props) => {
+  const [hasError, setHasError] = createSignal(false);
+
   const getArtifactData = async (content: string) => {
-    return await $typst.svg({ mainContent: content });
+    try {
+      const svg = await $typst.svg({ mainContent: content });
+      oldSvg = svg;
+      setHasError(false);
+      return svg;
+    } catch {
+      setHasError(true);
+      return oldSvg;
+    }
   };
 
   const [svg] = createResource(props.contentSignal, getArtifactData);
@@ -14,10 +31,13 @@ const SvgPreview: Component<{ contentSignal: Accessor<string> }> = (props) => {
       {svg() === undefined ? (
         "loading..."
       ) : (
-        <div
-          innerHTML={svg()}
-          style={{ border: "1px solid black", width: "fit-content" }}
-        ></div>
+        <>
+          {hasError() && <div>typst error...</div>}
+          <div
+            innerHTML={svg()}
+            style={{ border: "1px solid black", width: "fit-content" }}
+          />
+        </>
       )}
     </div>
   );
